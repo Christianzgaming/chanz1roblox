57c9915de517d8167ac23bab6429de9e2b81c86e
Christian Gaming <82629444+Christianzgaming@users.noreply.github.com>
2024-07-03 02:04:07 +0800

    Update ServerHOP.lua

--- ServerHOP.lua
+++ ServerHOP.lua
@@ -25,7 +25,7 @@ function TPReturner()
     for i,v in pairs(Site.data) do
         local Possible = true
         ID = tostring(v.id)
-        if tonumber(v.playing) <= 5 then
+        if tonumber(v.playing) <= 3 then
             for _,Existing in pairs(AllIDs) do
                 if num ~= 0 then
                     if ID == tostring(Existing) then
